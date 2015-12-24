e7cd5678e8b7edd66aed395d34b642fb3d4d9990
ixiaowei <879898421@qq.com>
2015-12-24 17:56:01 +0800

    新增数据库帮助类，以及各表对应的实体类

---
+++ src/db/CoolweatherOpenHelper.java
@@ -0,0 +1,20 @@
+package db;
+
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class CoolweatherOpenHelper extends SQLiteOpenHelper {
+
+	@Override
+	public void onCreate(SQLiteDatabase arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase arg0, int arg1, int arg2) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
