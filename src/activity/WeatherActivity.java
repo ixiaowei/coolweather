4ef6e5ab6181a3436c6462c036992984bb0bb468
ixiaowei <879898421@qq.com>
2015-12-25 18:43:49 +0800

    新增切换城市和手动更新天气的功能

--- src/activity/WeatherActivity.java
+++ src/activity/WeatherActivity.java
@@ -54,7 +54,8 @@ public class WeatherActivity extends Activity implements OnClickListener {
 	*/
 	private Button refreshWeather;
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	protected void onCreate(Bundle savedInstanceState) 
+	{
 	super.onCreate(savedInstanceState);
 	requestWindowFeature(Window.FEATURE_NO_TITLE);
 	setContentView(R.layout.weather_layout);
@@ -84,7 +85,8 @@ public class WeatherActivity extends Activity implements OnClickListener {
 	refreshWeather.setOnClickListener(this);
 	}
 	@Override
-	public void onClick(View v) {
+	public void onClick(View v) 
+	{
 	switch (v.getId()) {
 	case R.id.switch_city:
 	Intent intent = new Intent(this, ChooseAreaActivity.class);
